1be15700b2009193aa87a06b7c02f471c2464c09
Jia Xiang <gweejx@gmail.com>
2025-05-05 10:53:02 +0800

    trying for heroku again

--- recipeApp/server.js
+++ recipeApp/server.js
@@ -14,5 +14,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
