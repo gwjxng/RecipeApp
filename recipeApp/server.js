b07564913ed1fbff1ebcd01a406d40d894daa8f2
Jia Xiang <gweejx@gmail.com>
2025-05-05 11:08:05 +0800

    changed some files for heroku

--- recipeApp/server.js
+++ recipeApp/server.js
@@ -16,3 +16,5 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
+
+app.get('/favicon.ico', (req, res) => res.status(204).end());
